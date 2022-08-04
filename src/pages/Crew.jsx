78bd7244aed79f4b7ba634a7871c8b7a6bd06af0
Enes Akkose <ens.akkose@gmail.com>
2022-08-05 01:39:32 +0300

    improved tablet version of crew page

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -6,25 +6,27 @@ import './Crew.scss'
 function Crew() {
 
   const [no, setNo] = useState(0)
-  console.log(data.crew)
+
   return (
     <div className='crew'>
       <Header/>
         <div className="container">
-            
             <h5><span>02</span> MEET YOUR CREW</h5>
+
             <main className="crew__main">
               <div className="crew__main__info">
                 <h3>{data.crew[no].role.toUpperCase()}</h3>
                 <h1>{data.crew[no].name.toUpperCase()}</h1>
                 <p>{data.crew[no].bio}</p>
+
                 <div className="crew__main__info__slide">
                   {data.crew.map((item, index) => (
                     <button key={index} onClick={() => setNo(index)} className={`${index === no && 'activeSlide'}`}></button>
                   ))}
                 </div>
+
               </div>
-              <img src={data.crew[no].images.png}alt="" />
+              <img src={data.crew[no].images.png} alt="" />
             </main>
 
         </div>
--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -13,11 +13,13 @@ function Destination() {
     <div className='destination'>
         <Header/>
         <div className="container">
-            
             <h5><span>01</span>PICK YOUR DESTINATION</h5>
-            <main className="destination__main">
+
+          <main className="destination__main">
             <Img className='planet' src={data.destinations[no].images.png} alt={data.destinations[no].name} size='445' />
+
             <div className="destination__info">
+
               <nav className='destination__info__navbar'>
                 {data.destinations.map((destination, index) => (
                   <button className={`navbar-btn ${no === index && 'activeNav'}`} key={index} onClick={() => setNo(index)}>
@@ -26,20 +28,23 @@ function Destination() {
                   </button>
                 ))}
               </nav>
-              <h2>{data.destinations[no].name.toUpperCase()}</h2>
-              <p>{data.destinations[no].description}</p>
+
+                <h2>{data.destinations[no].name.toUpperCase()}</h2>
+                <p>{data.destinations[no].description}</p>
+
               <footer className='destination__info__footer'>
-                <div className="destination__info_footer__avg">
-                  <h6>AVG.DISTANCE</h6>
-                  <span>{data.destinations[no].distance}</span>
-                </div>
-                <div className="destination__info_footer__travel">
-                  <h6>EST.TRAVEL TIME</h6>
-                  <span>{data.destinations[no].travel}</span>
-                </div>
+                  <div className="destination__info_footer__avg">
+                    <h6>AVG.DISTANCE</h6>
+                    <span>{data.destinations[no].distance}</span>
+                  </div>
+
+                  <div className="destination__info_footer__travel">
+                    <h6>EST.TRAVEL TIME</h6>
+                    <span>{data.destinations[no].travel}</span>
+                  </div>
               </footer>
             </div>
-            </main>
+          </main>
         </div>
     </div>
   )

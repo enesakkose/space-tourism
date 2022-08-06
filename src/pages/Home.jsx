594571b737b893d15bd4105c7cfe3f4398782f1a
Enes Akkose <ens.akkose@gmail.com>
2022-08-07 01:40:33 +0300

    home page edited and finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,15 @@ function Home() {
     <div className='home'>
         <Header/>
         <div className="container">
-          
-          <main className='home__main'>
+          <main className="home__main">
             <div className="home__main__text">
               <h4>SO, YOU WANT TO TRAVEL TO</h4>
               <h1>SPACE</h1>
-              <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p> 
+              <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
             </div>
-            <ExploreBtn/>
+            <div className="home__main__btn">
+              <ExploreBtn/>
+            </div>
           </main>
         </div>
     </div>
